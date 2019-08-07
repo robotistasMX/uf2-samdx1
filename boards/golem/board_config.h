31b28d15894ec3061902abce21e644eb895e4522
Franco <gfdgeras@gmail.com>
2019-08-07 00:09:58 -0500

    Bootloader Golem

---
+++ boards/golem/board_config.h
@@ -0,0 +1,20 @@
+#ifndef BOARD_CONFIG_H
+#define BOARD_CONFIG_H
+
+#define CRYSTALLESS    1
+#define __SAMD21G18A__ 1
+
+#define VENDOR_NAME "Robotica Golem"
+#define PRODUCT_NAME "Golem"
+#define VOLUME_LABEL "GOLEM 2.0"
+#define INDEX_URL "http://roboticagolem.com/"
+
+#define USB_VID 0x1209   // Atmel
+#define USB_PID 0xBAB1   // Generic HID device
+
+#define BOARD_ID "golem-samd21g18a"
+
+#define LED_PIN PIN_PA17
+
+
+#endif
