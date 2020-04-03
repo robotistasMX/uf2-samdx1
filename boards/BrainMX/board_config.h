a230905fc5cf4717dfb8f4c487a0941eadf5bec0
Franco <gfdgeras@gmail.com>
2020-04-03 15:03:41 -0600

    BrainMX bootloader

---
+++ boards/BrainMX/board_config.h
@@ -0,0 +1,20 @@
+#ifndef BOARD_CONFIG_H
+#define BOARD_CONFIG_H
+
+#define CRYSTALLESS    1
+#define __SAMD21G18A__ 1
+
+#define VENDOR_NAME "RobotistasMX"
+#define PRODUCT_NAME "BrainMX"
+#define VOLUME_LABEL "BrainMX 1.0"
+#define INDEX_URL "https://robotistas.mx/"
+
+#define USB_VID 0x1209   // Atmel
+#define USB_PID 0xBFA1   // Generic HID device
+
+#define BOARD_ID "brainmx-samd21g18a"
+
+#define LED_PIN PIN_PA17
+
+
+#endif
